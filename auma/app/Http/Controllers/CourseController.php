c6851723a834c6883018e197c537a8f5d25bb1ff
abdosaned40 <153099139+abdosaned40@users.noreply.github.com>
2023-12-22 16:03:17 +0200

    fffff
    fffffffffvc

--- auma/app/Http/Controllers/CourseController.php
+++ auma/app/Http/Controllers/CourseController.php
@@ -36,16 +36,18 @@ class CourseController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Course $course)
     {
-        //
+        $course->update($request->all());
+        return response()->json($course);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Course $course)
     {
-        //
+        $course->delete();
+        return response()->json(null, 204);
     }
 }
