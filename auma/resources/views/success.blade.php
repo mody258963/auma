453b4cb6459d61c722afef574f35eade99aa9c22
abdosaned40 <153099139+abdosaned40@users.noreply.github.com>
2024-01-19 07:42:46 +0200

    go
    dodo

---
+++ auma/resources/views/success.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Password Successfully </title>
+</head>
+
+<style>
+
+    h1{
+        color: green;
+        text-align: center;
+    }
+
+    img{
+
+        margin-left:35%;
+        margin-top: 35px;
+    }
+
+    footer{
+        text-align: center;
+        margin-top: 15px;
+        color: green;
+    }
+</style>
+<body>
+    <h1>Your Password Reset Successfully </h1>
+
+    <img src="https://images.pexels.com/photos/3824771/pexels-photo-3824771.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" srcset="">
+    <footer>
+        Auma @2024
+    </footer>
+</body>
+</html>
